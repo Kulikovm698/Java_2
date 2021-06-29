aaa63870bf309d6a15aca2d1ad7f5d3fe983aabb
Maxim <kulikovm698@gmail.com>
2021-06-29 22:59:08 +0300

    Test init-2

--- src/main/java/Test/Main.java
+++ src/main/java/Test/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        System.out.println("Hello_2");
+        System.out.println("Hello_3");
+        System.out.println("Hello_4");
 
     }
 }
